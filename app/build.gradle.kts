c1dceb711487d14e479cc70586226d978383ce8e
Leo Lennards <leo.lukelennards@gmail.com>
2026-09-26 07:20:13 +0000

    Bump versionCode to 3 for the AD_ID manifest fix release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.leolennards.apexfitness"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
